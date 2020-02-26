c5d77099cfb660471e82275fd939fd65eb86afd6
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2020-02-27 01:38:24 +0200

    finish menu

--- lib/screens/Menu.dart
+++ lib/screens/Menu.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:restx/screens/Constants.dart';
 
 class Menu extends StatefulWidget {
   @override
@@ -11,10 +14,40 @@ class _MenuState extends State<Menu> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Menu"),
+        actions: <Widget>[
+          FlatButton(
+            child: Icon(Icons.settings_power),
+            onPressed: () {
+              FirebaseAuth.instance.signOut().then((result) {
+                navigatorKey.currentState.pushReplacementNamed(ROUTE_AUTH);
+              });
+            },
+          )
+        ],
       ),
-      body: Center(
-        child: Text("Menu"),
-      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection('menu').snapshots(),
+          builder: (context, snapshot) {
+            return ListView(
+                children: snapshot.data.documents.map((document) {
+              print(document['name']);
+              print(document['price']);
+              return Card(
+                child: StreamBuilder<QuerySnapshot>(
+                    stream: document.reference.collection('items').snapshots(),
+                    builder: (context, snapshot) {
+                      return ListTile(
+                        title: Text(document['name']),
+                        subtitle: Text("${document['price']} L.E"),
+                        leading: SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: Image.network(document['img'])),
+                      );
+                    }),
+              );
+            }).toList());
+          }),
     );
   }
 }
