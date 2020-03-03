51704f46bab35760062e589d18bae19d037016f2
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2020-03-03 08:03:24 +0200

    add change table

--- lib/screens/Menu.dart
+++ lib/screens/Menu.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:restx/screens/Constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Menu extends StatefulWidget {
   @override
@@ -16,13 +17,24 @@ class _MenuState extends State<Menu> {
         title: Text("Menu"),
         actions: <Widget>[
           FlatButton(
-            child: Icon(Icons.settings_power),
-            onPressed: () {
-              FirebaseAuth.instance.signOut().then((result) {
-                navigatorKey.currentState.pushReplacementNamed(ROUTE_AUTH);
-              });
+            child: Icon(
+              Icons.time_to_leave,
+              color: Colors.white,
+            ),
+            onPressed: () async {
+              await (await SharedPreferences.getInstance())
+                  .setString(TABLE_NUMBER, null);
+              await FirebaseAuth.instance.signOut();
+              navigatorKey.currentState.pushReplacementNamed(ROUTE_AUTH);
             },
-          )
+          ),
+          FlatButton(
+            child: Icon(
+              Icons.swap_calls,
+              color: Colors.white,
+            ),
+            onPressed: _onChangeTable,
+          ),
         ],
       ),
       body: StreamBuilder<QuerySnapshot>(
@@ -48,6 +60,55 @@ class _MenuState extends State<Menu> {
               );
             }).toList());
           }),
+    );
+  }
+
+  void _onChangeTable() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            color: Color(0xFF737373),
+            height: 50,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).canvasColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  )),
+              child: _buildChangeTableBottomSheet(),
+            ),
+          );
+        });
+  }
+
+  Widget _buildChangeTableBottomSheet() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        FlatButton(
+          child: Icon(
+            Icons.swap_calls,
+            color: Colors.teal,
+            size: 5,
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, ROUTE_QR);
+          },
+        ),
+        FlatButton(
+          child: Icon(
+            Icons.power_settings_new,
+            color: Colors.redAccent,
+          ),
+          onPressed: () async {
+            await (await SharedPreferences.getInstance())
+                .setString(TABLE_NUMBER, null);
+            Navigator.pushReplacementNamed(context, ROUTE_QR);
+          },
+        ),
+      ],
     );
   }
 }
