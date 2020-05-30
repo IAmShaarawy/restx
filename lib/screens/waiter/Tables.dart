a8128984bcd40bb592aba8a9774051bbd3e8697c
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2020-05-30 18:34:59 +0200

    add tables to waiter

--- lib/screens/waiter/Tables.dart
+++ lib/screens/waiter/Tables.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 class Tables extends StatefulWidget {
   @override
   _TablesState createState() => _TablesState();
@@ -7,6 +9,30 @@ class Tables extends StatefulWidget {
 class _TablesState extends State<Tables> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Tables"),);
+    return StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('tables').orderBy("number").snapshots(),
+        builder: (context, snapshot) {
+          return ListView(
+              children: snapshot.data.documents.map((document) {
+            return Card(
+              child: ListTile(
+                onTap: goToTable,
+                title: Text("Table # ${document['number']}"),
+                enabled: document['current_user'] != null,
+                subtitle: formTableSubTitle(document),
+              ),
+            );
+          }).toList());
+        });
   }
+
+  Widget formTableSubTitle(DocumentSnapshot doc) {
+    if (doc["current_user"] == null) return null;
+
+    int platesCount = (doc["plates"] as List).length;
+
+    return Text("$platesCount plates");
+  }
+
+  void goToTable() {}
 }
